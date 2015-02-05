463bf42e5558853956faf6096160a6f4b87828a9
Mateusz Korszun <mkorszun@gmail.com>
2015-02-05 20:30:51 +0100

    WIP

--- 9env.sh
+++ 9env.sh
@@ -9,11 +9,11 @@ apt-get -y install build-essential ruby-dev pkg-config
 apt-get -y install openjdk-7-jdk 
 gem install fpm
 
-tar -xvvf /vagrant/misc/jdk-1.8.0-openjdk-x86_64-1.8.0_u60-b01.tar.xz .
+tar -xvvf /vagrant/misc/jdk-1.8.0-openjdk-x86_64-1.8.0_u60-b01.tar.xz
 
 mkdir ~/openjdkathome
 cd ~/openjdkathome
-git clone https://github.com/hgomez/obuildfactory.git
-export JAVA_HOME=/vagrant/jdk1.8.0_60
+git clone -b fix-openjdk9-package-script https://github.com/mkorszun/obuildfactory.git
+export JAVA_HOME=/home/vagrant/jdk1.8.0_60
 XPACKAGE_MODE=generic XPACKAGE=true XCLEAN=true XUSE_NEW_BUILD_SYSTEM=true XBUILD=true ./obuildfactory/openjdk9/linux/standalone-job.sh
 cp -rf /root/openjdkathome/OBF_DROP_DIR/openjdk9 /vagrant
